00a9047e03d6f177fc3fb7a86fc1dada6d86af94
Andrew Li <andrewliyanlap000824@gmail.com>
2023-05-12 00:37:51 +0800

    ✨Simple UI for question and answer

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,54 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import {
+  Box,
+  AbsoluteCenter,
+  VStack,
+  Button,
+  Textarea,
+  Text,
+  Center,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Home() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const [isAsked, setIsAsked] = useState(false);
+  const [isThinking, setIsThinking] = useState(false);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <Box position="relative" h="100vh">
+        <AbsoluteCenter axis="both" w="100%" p="32px">
+          <Center>
+            <VStack spacing={2} w={["100%", "960px"]}>
+              <Textarea
+                variant="filled"
+                placeholder="Ask a question"
+                w="100%"
+                value={question}
+              />
+              <Button
+                isLoading={isThinking}
+                colorScheme="teal"
+                variant="solid"
+                w="100%"
+              >
+                Send
+              </Button>
+            </VStack>
+          </Center>
+          {isAsked && (
+            <Center pt="32px">
+              <Text fontSize="lg" w={["100%", "1096px"]}>
+                🤖: {answer}
+              </Text>
+            </Center>
+          )}
+        </AbsoluteCenter>
+      </Box>
     </main>
-  )
+  );
 }
